6b8a2372e4068f3f4e703f12c3b3c1e505ae0723
Wennan Huang <hwn1997@yahoo.com>
2020-10-24 18:53:11 -0500

    kkk

--- MP2/DataNode.py
+++ MP2/DataNode.py
@@ -1,59 +1,206 @@
-import InterNode_Comm
 import threading
 import time
-
+import sys
+import DataNodeServer as DataNodeServer
 
 
 import socket
 
 MAXSIZE = 1024
 
+DATANODE_PORT           = 8080
+#DATANODE_SERVER_PORT    = 8089
+MASTERNODE_SERVER_PORT  = 8099
+"""
+    TEST MODE:
+        node_id -> ('localhost', node_id)
+
+    In real, it should be
+        node_id -> (node_id, constant_server_port)
+
+"""
+def findNodeAddress(node_id):
+    return ('localhost', node_id)
+    # return (node_id, DATANODE_SERVER_PORT)
+
 class DateNode:
     def __init__(self, node_id, address):
 
         # id and address
         self.node_id = node_id
-        self.address = address
+
+
+        # FOR TEST ('localhost', port)
+        self.address_ip     = address[0]
+        self.address_port   = int(address[1])
 
         # membershipList and fileList
 
         # master elector related information stored in
-        self.membershipList = MemberShipList()
-        self.fileList       = FileList()
-        self.electionList = dict()
-        self.in_progress = False
-        # TODO: Heartbeats Set up
-        Heartbeat_process = HeartbeatSetUp()
+        #self.membershipList = MemberShipList()
+        #self.fileList       = FileList()
+        #self.electionList = dict()
 
 
-        # TODO: Lock and critical information
-        # master, lo
-        self.masterAddress = (ip port)
 
+        # real master address should be choose by a elector function
+        ###### FOR TEST ONLY ######
+        self.master_address = ('localhost', MASTERNODE_SERVER_PORT)
+        ###### FOR TEST ONLY ######
+
+
+        # create a DataNode Server, let it keep running
+        self.datanode_server = DataNodeServer.DataNodeServer(server_address= (self.address_ip, self.address_port+9))
+
+        self.thread_server  = threading.Thread(target= self.Maintain_server,
+                                                 daemon= True)
+        self.thread_server.start()
+
+
+
+        self.keyboard_listener = self.Keyboard_Listener(BroadcastModeLock= None, SenderQueue= None)
+
+
+    def Keyboard_Listener(self, BroadcastModeLock, SenderQueue):
+
+        print("start keyboard listening\n")
+
+        while True:
+
+            # Read from the KeyBoard Input
+
+            keyboard_cmd = input("Waiting for CMD\n")
+            keyboard_cmd = keyboard_cmd.split(' ')
+
+            # parser the keyboard_cmd:
+            try:
+
+                """
+                ops for MP2
+                """
+
+                # "put localfilename sdfsfilename"
+                if keyboard_cmd[0] == "put":
+
+                    (ops, localfilename, sdfsfilename) = keyboard_cmd
+                    print("running " + ops + "cmd")
+                    self.PUT(localfilename= localfilename, sdfsfilename= sdfsfilename)
+
+                # "get sdfsfilename localfilename"
+                elif keyboard_cmd[0] == "get":
+                    (ops, sdfsfilename, localfilename) = keyboard_cmd
+                    self.GET(localfilename= localfilename, sdfsfilename=sdfsfilename)
+
+                # "delete sdfsfilename"
+                elif keyboard_cmd[0] == "delete":
+                    (ops, sdfsfilename) = keyboard_cmd
+                    self.DELETE(sdfsfilename= sdfsfilename)
+
+
+                else:
+                    print("WRONG CMD, PLEASE RETRY")
+
+                # # "ls sdfsfilename"
+                # elif keyboard_cmd[0] == "ls":
+                #     (ops, sdfsfilename) = keyboard_cmd
+                #     self.LS(sdfsfilename = sdfsfilename)
+                #
+                # # "store"
+                # elif keyboard_cmd[0] == "store":
+                #     self.STORE()
+
+                # # CMD: show MyNode_d
+                # if CMD == "show MyID":
+                #     print("My node_id is: " + self.node_id)
+                #
+                # # CMD: show MyList
+                # elif CMD == "show MyList":
+                #     print(self.MyList)
+                #
+                # # CMD: switch broadcast mode to ALL2ALL
+                # elif CMD == "switch to ALL2ALL":
+                #     BroadcastModeLock.acquire()
+                #     try:
+                #         self.isGossip = False
+                #     finally:
+                #         BroadcastModeLock.release()
+                #
+                # # CMD: switch broadcast mode to GOSSIP
+                # elif CMD == "switch to GOSSIP":
+                #     BroadcastModeLock.acquire()
+                #     try:
+                #         self.isGossip = True
+                #     finally:
+                #         BroadcastModeLock.release()
+                #
+                # # CMD: show the current broadcast
+                # elif CMD == "show curr_mode":
+                #     BroadcastModeLock.acquire()
+                #     try:
+                #         print("Gossip") if self.isGossip else print("ALL2ALL")
+                #     finally:
+                #         BroadcastModeLock.release()
+
+
+
+                """
+                MP1:    KILL
+                        LEFT
+                        JOIN
+                """
+
+                # # CMD: KILL node_i
+                # elif keyboard_cmd[0] == "KILL":
+                #     self.stillAlive = False
+                #     sys.exit()
+                #
+                # # CMD: LEFT node_i
+                # elif keyboard_cmd[0] == "LEFT":
+                #     self.LeftAction()
+                #     # set isInGroup to False
+                #     self.isInGroup.clear()
+                #
+                # # CMD: JOIN
+                # elif keyboard_cmd[0] == "JOIN":
+                #     self.JoinAction()
+                #     # set isInGroup to True
+                #     self.isInGroup.set()
+
+
+
+
+
+            except:
+                print("SOMETHING WRONG IN RUNNING THIS CMD")
+
+
+
+
+    def Maintain_server(self):
+        self.datanode_server.serve_forever()
 
 
 
     def PUT(self, localfilename, sdfsfilename):
-
-
         # A thread do the PUT work
         def threading_putwork(sdfsfilename):
 
-            last_failed = False
+            print("a thread work has begin working")
+
+            last_failed = True
             last_node = None
 
-            # a client only connect to master
-            master_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            master_client.connect(master_client)
+
 
             while last_failed:
 
                 # e.g. "ASSIGN a.txt"
                 msg = "ASSIGN "+sdfsfilename
-                master_client.send(msg.encode())
 
-                response = master_client.recv(MAXSIZE).decode()
-                master_client.close()
+
+                response = self.request_from_master(msg)
+
+
                 # can't PUT right now
                 if response == "FILE IN USING":
                     print(response)
@@ -64,73 +211,94 @@ class DateNode:
                     exit()
 
                 else:
-                    peer_node_address = findNodeAddress(response)
+                    # START PEER TRANSFER
 
-                    # a client communicate with peer node to transfer
+                    peer_server_address = self.get_peerserver_address(response)
+                    print("peer_server_ip: "    + peer_server_address[0])
+                    print("peer_server_port: "  + str(peer_server_address[1]))
                     try:
                         peer_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        peer_client.connect(peer_node_address)
+                        peer_client.connect(peer_server_address)
 
-                        # send "SEND a.txt" to peer_client
-                        msg = "SEND "+sdfsfilename
+                        # send "PUT a.txt" to peer_client
+                        msg = "PUT "+sdfsfilename
                         peer_client.send(msg.encode())
 
                     except:
                         print("can't connect peer node")
 
-                    localfile = open(localfilename)
+                    localfile = open(localfilename, "rb")
 
-                    try:
+                    # try:
+                    #
+                    #     print("connect peer node " + response + " start trans")
+                    #
+                    #     content = localfile.read(MAXSIZE)
+                    #     while content:
+                    #             print("Send data:{}".format(content).encode())
+                    #             peer_client.send(content)
+                    #             content = localfile.read(MAXSIZE)
+                    #
+                    #     localfile.close()
+                    #     peer_client.close()
+                    #
+                    #     print("a thread to "+ response +" finished SENDING JOB")
+                    #     last_failed = False
+                    #
+                    #     # confirm with master
+                    #     msg = "CONFIRM "+sdfsfilename + " " + response
+                    #     self.request_from_master(msg)
+                    #
+                    # except:
+                    #     print("peer node "+ response +" is failed")
+                    #     last_failed = True
+                    #     print("now repeating")
 
+                    print("connect peer node " + response + " start trans")
+
+                    content = localfile.read(MAXSIZE)
+                    while content:
+                        print("Send data:{}".format(content).encode())
+                        peer_client.send(content)
                         content = localfile.read(MAXSIZE)
-                        while content:
-                                peer_client.send(content)
-                                content = localfile.read(MAXSIZE)
 
-                        localfile.close()
-                        peer_client.close()
+                    localfile.close()
+                    peer_client.close()
 
-                        print("a thread to "+peer_node_address+"finished SENDING JOB")
-                        last_failed = False
+                    print("a thread to " + response + " finished SENDING JOB")
+                    last_failed = False
 
-                        # send a CONFIRM to master
-                        master_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        master_client.connect(master_client)
-
-                        msg = "CONFIRM "+sdfsfilename + " " + response
-                        master_client.send(msg.encode())
-
-                        master_client.close()
-
-                    except:
-                        print("peer node "+peer_node_address+" is failed")
-                        last_failed = True
-                        last_node   = peer_node_address
-                        print("now repeating")
+                    # "CONFIRM [node_id] [filename]"
+                    msg = "CONFIRM " + response + " " + sdfsfilename
+                    self.request_from_master(msg)
 
 
 
 
 
-        f = open(localfilename)
+        f = open(localfilename, "rb")
         if f is None:
             print("CAN'T FIND LOCAL FILE")
             return
 
+        # create a pool to control the thread
+        thread_pool = []
         try:
-            for i in range(4):
-                threading_putworker(sdfsfilename)
+            for i in range(1):
+                thread_putworker = threading.Thread(target= threading_putwork,
+                                                    args=(sdfsfilename,))
+                thread_putworker.start()
+
+                thread_pool.append(thread_putworker)
 
         finally:
             f.close()
-
-
 
     def GET(self, localfilename, sdfsfilename):
         try:
             # a client only connect to master
             master_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            master_client.connect(master_client)
+            master_client.connect(self.get_masterserver_address())
 
             # e.g. "FIND a.txt"
             msg = "FIND " + sdfsfilename
@@ -166,7 +334,7 @@ class DateNode:
                 peer_client.send(msg.encode())
 
                 # write to local file
-                f = open(localfilename)
+                f = open(localfilename, "wb")
                 content = peer_client.recv(MAXSIZE)
                 while content:
                     f.write(content)
@@ -178,22 +346,64 @@ class DateNode:
                 print("one peer node" + avail_node + "failed")
                 print("NOW loop for next")
 
-
     def DELETE(self, sdfsfilename):
         try:
             # a client only connect to master
             master_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            master_client.connect(master_client)
+            master_client.connect(self.get_masterserver_address())
 
-            # e.g. "FIND a.txt"
+            # e.g. "PURGE a.txt"
             msg = "PURGE " + sdfsfilename
             master_client.send(msg.encode())
-
             response = master_client.recv(MAXSIZE).decode()
             master_client.close()
         except:
             print("can't connect to master node")
 
+    # a temp client talk to master
+    def request_from_master(self, msg):
+        # a client only connect to master
+        master_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        master_client.connect(self.get_masterserver_address())
 
-    # given node_id, return (node_ip, node_port)
-    def findNodeAddress(self, node_id):
+        print("sending to master: " + msg)
+        master_client.send(msg.encode())
+
+        response = master_client.recv(MAXSIZE).decode()
+        print("receive from master: " + response)
+        master_client.close()
+
+        return response
+
+
+
+
+    # in real, this should wait for the master_address if there is no available one
+    def get_masterserver_address(self):
+
+        if self.master_address is None:
+            pass
+
+        address = ('fa20-cs425-g29-01.cs.illinois.edu', 9999)
+        return self.master_address
+
+    # in real, node_id is 'fa20-cs425-g29-01.cs.illinois.edu'
+    # in test, node_id is the port '8080',
+
+    def get_peerserver_address(self, peer_node_id):
+        # need to return server's address
+        peerserver_address = ('localhost', int(peer_node_id) + 9)
+
+        return peerserver_address
+
+
+
+if __name__ == "__main__":
+
+    port = sys.argv[1]
+
+    address = ('localhost', port)
+    print("this datanode ip is: "+ address[0])
+    print("this datanode port is: " + address[1])
+    datanode = DateNode("test", address)
+
--- MP2/DataNodeServer.py
+++ MP2/DataNodeServer.py
@@ -1,48 +1,240 @@
-
 import socketserver
 import socket
-import InterNode_Comm
+import threading
+# import MembershipList
+from random import shuffle
+from collections import defaultdict
+import sys
+
 MAXSIZE = 1024
 
+
+DATANODE_SERVER_PORT    = 8089
+MASTERNODE_SERVER_PORT  = 8099
+
 """
-    This DataNodeServer should help the server inside each DataNode in follow request:
-        1. a PUT request from other DataNode
-        2. a GET request from other DataNode
-        3. a DELETE request from MasterNode
+    TEST MODE:
+        node_id -> ('localhost', node_id)
+
+    In real, it should be
+        node_id -> (node_id, constant_server_port)
+
 """
+def findNodeAddress(node_id):
+    return ('localhost', node_id)
+    # return (node_id, DATANODE_SERVER_PORT)
+
+
+
+"""
+        MasterNode operate as:
+            1.  A extra process calling Master Server.
+            2.  A daemon process checking FileList, send out each time
+
+        All request are handled by MasterHandle, including the following case:
+
+        1.  "ASSIGN [filename]"
+            The first step of PUT
+            Choose 4 node, add (key:filename, value: [nodes, ING/SUCC/FAIL])
+            * pay attention to lock part.
+
+        2.  "FIND [filename]"
+            The first step of GET, check the FileList and 
+                if can't find, return "404 NOT FOUND"
+            * don't forget the lock
+
+        3.  "PURGE [filename]"
+            check the fileList, find all Storage Node and tell to "DELETE a.txt"
+            * lock, lock, lock
+
+        4.  "CONFIRM [node_id] [filename]"
+            sent by the PUT NODE, mark a confirmation of a replication of a.txt
+            when 4 storage confirmed, mark this file successful uploaded, and write it into FileList
+
+        5.  "REASSIGN [node_id] [filename]"
+            similar to assign case, however, make this node_id as failed, then assign a new one
+
+"""
+
+
 class DataNodeServerHandler(socketserver.BaseRequestHandler):
 
 
-    def __init__(self):
-        pass
+    def __init__(self, request, client_address, server):
+        socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
+        return
 
 
+    # TODO Check encode/decode case
     def handle(self):
-        self.data = self.request.recv(MAXSIZE)
+        data = self.request.recv(MAXSIZE).decode()
+
+        print("Receive data:{}".format(data))
 
         # praser
-        # example: "PUT a.txt/n"
-        if self.data[:3] == "PUT":
-            filename = self.data[4:]
+        # example: "ASSIGN a.txt"
+        reply = ""
+        # from_host is the hostname of the client.
+        from_host = socket.gethostbyaddr(self.client_address[0])[0]
 
-            # perhaps a new thread to handle this one
-            InterNode_Comm.readSocket_writeLocal(socket, filename)
+        splits = data.split(' ')
 
-        # "GET a.txt/n"
-        elif self.data[:3] == "GET":
-            filename = self.data[4:]
+        # "GET a.txt"
+        if splits[0] == "GET":
+            self.GET(splits[1])
 
-            # check file exists, otherwise reply with error code
+        # "PUT a.txt"
+        elif splits[0] == "PUT":
+            self.PUT(splits[1])
 
-            # perhaps a new thread to handle this one
-            InterNode_Comm.readLocal_writeSocket(socket, filename)
+        # "SEND a.txt node_id"
+        elif splits[0] == "SEND":
+            self.SEND(splits[1], splits[2])
 
-        #  01234567890
-        # "DELETE a.txt"
-        elif self.data[:6] == "DELETE":
-            filename = self.data[7:]
 
-            # check file exists, if exists, delete
 
-            # reply with "CONFIRM DELETE a.txt"
+        # server handle "GFT sdfsfilename"
+    def GET(self, sdfsfilename):
 
+        # "r" - read mode
+        sdfsfile = open(sdfsfilename, "rb")
+
+        # read form sdfsfile, write into socket
+        content = sdfsfile.read(MAXSIZE)
+        while content:
+            self.request.send(content)
+            content = sdfsfile.read(MAXSIZE)
+
+        sdfsfile.close()
+        return
+
+
+
+        # server handle "PUT sdfsfilename"
+    def PUT(self, sdfsfilename):
+
+        print("server start a put job, sdfsilename:" + sdfsfilename)
+
+        # "w" - overwrite
+        sdfsfile = open(sdfsfilename, "wb")
+
+
+        print("open ")
+
+        # read from socket, write into sdfs
+        content = self.request.recv(MAXSIZE)
+        while content:
+            print("Receive data:{}".format(content.decode()))
+
+            sdfsfile.write(content)
+            content = self.request.recv(MAXSIZE)
+
+        sdfsfile.close()
+
+        print("put job: " +sdfsfilename + "finished")
+
+        return
+
+
+
+
+
+
+
+
+        # MasterNode client send "SEND sdfsfilename node_id"
+        # DateNode will send sdfsfile to Target DataNode
+        #  - if successful, reply "BACKUP sdfsfilename node_id"
+        #  - if Target DataNode failed, reply "FAIL_BACKUP sdfsfilename node_id"
+        #       to master node  and finish this request
+        # if this node failed, let MasterNode decide the next action
+    def SEND(self, sdfsfilename, target_DataNode):
+
+        peer_node_address = findNodeAddress(target_DataNode)
+
+        sdfsfile = open(sdfsfilename, "rb")
+
+        # a client communicate with peer node to transfer
+        try:
+            # ++++++ a peer client to target_Datanode   ++++++
+            peer_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            peer_client.connect(peer_node_address)
+
+            # send "PUT a.txt" to peer_client
+            msg = "PUT " + sdfsfilename
+            peer_client.send(msg.encode())
+
+            content = sdfsfile.read(MAXSIZE)
+            while content:
+                peer_client.send(content)
+                content = sdfsfile.read(MAXSIZE)
+
+            sdfsfile.close()
+            peer_client.close()
+
+            print("a thread to " + peer_node_address + "finished SENDING JOB")
+            # ++++++        already back up             ++++++
+
+            # send a confirm information to master
+            msg = "BACKUP " + sdfsfilename + target_DataNode
+            self.request.send(msg.encode())
+
+        except:
+            # backup failed, send an information to master
+            msg = "FAIL_ACKUP " + sdfsfilename + target_DataNode
+            self.request.send(msg.encode())
+
+# Similar to lib example
+class DataNodeServer(socketserver.TCPServer):
+
+    # server_address = (localhost, port)
+    def __init__(self, server_address, handler_class=DataNodeServerHandler):
+
+        print("this server ip is: " + str(server_address[0]))
+        print("this server port is: " + str(server_address[1]))
+        socketserver.TCPServer.__init__(self, server_address, handler_class)
+        return
+
+    def server_activate(self):
+        #self.logger.debug('server_activate')
+        socketserver.TCPServer.server_activate(self)
+        return
+
+    def serve_forever(self):
+        #self.logger.debug('waiting for request')
+
+        print("serve is running")
+        #self.logger.info('Handling requests, press <Ctrl-C> to quit')
+        while True:
+            self.handle_request()
+        return
+
+    def handle_request(self):
+        #self.logger.debug('handle_request')
+        return socketserver.TCPServer.handle_request(self)
+
+    def verify_request(self, request, client_address):
+        #self.logger.debug('verify_request(%s, %s)', request, client_address)
+        return socketserver.TCPServer.verify_request(self, request, client_address)
+
+    def process_request(self, request, client_address):
+        #self.logger.debug('process_request(%s, %s)', request, client_address)
+        return socketserver.TCPServer.process_request(self, request, client_address)
+
+    def server_close(self):
+        #self.logger.debug('server_close')
+        return socketserver.TCPServer.server_close(self)
+
+    def finish_request(self, request, client_address):
+        #self.logger.debug('finish_request(%s, %s)', request, client_address)
+        return socketserver.TCPServer.finish_request(self, request, client_address)
+
+    def close_request(self, request_address):
+        #self.logger.debug('close_request(%s)', request_address)
+        return socketserver.TCPServer.close_request(self, request_address)
+
+#
+#
+# if __name__ == '__main__':
+#     # nope, don't test this server alone
+#     # integrate this into DataNode and test together
